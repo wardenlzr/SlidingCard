c5f33768e000f50af5bb5282f9d7130979689a73
yubin <wardenlzr@qq.com>
2019-03-02 11:31:31 +0800

    替换新版的AndroidJavaCompile

--- app/src/main/java/com/bg/slidingcard/aop_test/TestAnnoAspect.java
+++ app/src/main/java/com/bg/slidingcard/aop_test/TestAnnoAspect.java
@@ -1,10 +1,8 @@
 package com.bg.slidingcard.aop_test;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 /**
